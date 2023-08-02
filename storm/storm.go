61874eba8f605c4abc2c48736ee60281cc5a7226
lizexin <zexin-li@outlook.com>
2023-08-02 19:59:00 +0800

    新增连接丢失统计信息，不再打印单个连接丢失信息

--- storm/storm.go
+++ storm/storm.go
@@ -5,21 +5,63 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/sirupsen/logrus"
 	"github.com/timeway/mqtt-storm/mocker"
+	"strings"
 	"sync"
 	"time"
 )
 
 type MqttStorm struct {
 	sync.RWMutex
-	MqttClientMap map[string]mqtt.Client
-	Mocker        mocker.Mocker
-	started       bool
+	MqttClientMap     map[string]mqtt.Client
+	Mocker            mocker.Mocker
+	started           bool
+	connectLostCounts sync.Map
 }
 
 func NewMqttStorm(mocker mocker.Mocker) *MqttStorm {
 	return &MqttStorm{
 		MqttClientMap: make(map[string]mqtt.Client),
 		Mocker:        mocker,
+	}
+}
+
+func Observe(ms *MqttStorm) {
+	lastClientSize := 0
+	lastPrintTime := time.Now()
+	lastConnectLostCounts := ""
+	for ms.started {
+		currClientSize := len(ms.MqttClientMap)
+		if currClientSize != lastClientSize || time.Now().Sub(lastPrintTime) > time.Duration(10)*time.Second {
+			logrus.Infof("clientSize: %d", len(ms.MqttClientMap))
+			lastClientSize = currClientSize
+			lastPrintTime = time.Now()
+		}
+
+		// 连接丢失统计信息
+		currConnectLostCounts := ""
+		ms.connectLostCounts.Range(func(errInfo, count any) bool {
+			currConnectLostCounts += fmt.Sprintf("\n%d ===> %s", count.(int32), errInfo.(string))
+			return true
+		})
+		if currConnectLostCounts != lastConnectLostCounts {
+			logrus.Infof("连接丢失统计: %s", currConnectLostCounts)
+			lastConnectLostCounts = currConnectLostCounts
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (ms *MqttStorm) Run(clientCount uint64) {
+	if ms.started {
+		return
+	}
+	ms.started = true
+	go Observe(ms)
+
+	addClientErr := ms.AddClientByTargetCount(clientCount)
+	if addClientErr != nil {
+		logrus.Errorf("AddClientByTargetCount error: %s", addClientErr.Error())
 	}
 }
 
@@ -35,36 +77,9 @@ func (ms *MqttStorm) Shutdown() {
 	logrus.Infof("shutdown storm finish(client size: %d)", mqttClientSize)
 }
 
-func (ms *MqttStorm) Run(clientCount uint64) {
-	if ms.started {
-		return
-	}
-	ms.started = true
-	go func() {
-		lastClientSize := 0
-		lastPrintTime := time.Now()
-		for ms.started {
-			currClientSize := len(ms.MqttClientMap)
-			if currClientSize != lastClientSize || time.Now().Sub(lastPrintTime) > time.Duration(10)*time.Second {
-				logrus.Infof("clientSize: %d", len(ms.MqttClientMap))
-				lastClientSize = currClientSize
-				lastPrintTime = time.Now()
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	addClientErr := ms.AddClientByTargetCount(clientCount)
-	if addClientErr != nil {
-		logrus.Errorf("AddClientByTargetCount error: %s", addClientErr.Error())
-	}
-}
-
-var CountLessZero = fmt.Errorf("count <= 0")
-
 func (ms *MqttStorm) AddClientByTargetCount(targetCount uint64) error {
 	if targetCount <= 0 {
-		return CountLessZero
+		return fmt.Errorf("count <= 0")
 	}
 
 	ms.Lock()
@@ -87,9 +102,10 @@ func (ms *MqttStorm) AddClientByTargetCount(targetCount uint64) error {
 
 		go func() {
 			if connectToken.Wait() && connectToken.Error() != nil {
+				addDisconnectReasonCount(ms, connectToken.Error().Error())
+
 				ms.Lock()
 				defer ms.Unlock()
-				logrus.Errorf("mqttClient[%s] conn err: %s", clientId, connectToken.Error().Error())
 				delete(ms.MqttClientMap, clientId)
 			}
 
@@ -171,16 +187,13 @@ func (ms *MqttStorm) newMqttClient() (mqtt.Client, mqtt.Token, error) {
 	// 包装 ConnectionLostHandler
 	connectionLostHandler := options.OnConnectionLost
 	options.SetConnectionLostHandler(func(client mqtt.Client, err error) {
+		errInfo := err.Error()
+		addDisconnectReasonCount(ms, errInfo)
+
 		ms.Lock()
 		defer ms.Unlock()
 
 		optionsReader := client.OptionsReader()
-		errInfo := fmt.Sprintf("Client[%s] connection lost, error: %s", optionsReader.ClientID(), err.Error())
-		if "pingresp not received, disconnecting" == err.Error() {
-			logrus.Error(errInfo)
-		} else {
-			logrus.Warn(errInfo)
-		}
 		delete(ms.MqttClientMap, optionsReader.ClientID())
 		if connectionLostHandler != nil {
 			connectionLostHandler(client, err)
@@ -199,6 +212,18 @@ func (ms *MqttStorm) newMqttClient() (mqtt.Client, mqtt.Token, error) {
 	client := mqtt.NewClient(options)
 	token := client.Connect()
 	return client, token, nil
+}
+
+func addDisconnectReasonCount(ms *MqttStorm, errInfo string) {
+	writeConnectionResetByPeer := "write: connection reset by peer"
+	if strings.Contains(errInfo, writeConnectionResetByPeer) {
+		errInfo = writeConnectionResetByPeer
+	}
+
+	oldCount, _ := ms.connectLostCounts.LoadOrStore(errInfo, int32(0))
+	for !ms.connectLostCounts.CompareAndSwap(errInfo, oldCount, oldCount.(int32)+1) {
+		oldCount, _ = ms.connectLostCounts.LoadOrStore(errInfo, int32(0))
+	}
 }
 
 func (ms *MqttStorm) SubStorm() (int32, int32, error) {
